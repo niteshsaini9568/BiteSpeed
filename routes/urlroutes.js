c29d03e58e3552999f05fdfabcbb5bae865650ba
Nitesh saini <itsniteshsaini6376@gmail.com>
2026-03-03 22:26:26 +0530

    Just add the GET req for welcome message

--- routes/urlroutes.js
+++ routes/urlroutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainController");
 
+router.get("/", (req, res) => {
+    res.send("Welcome to the ByteSpeed Backend Task - Identity Reconciliation API, Hope you read the complete readme file to understand the task.");
+});
+
 router.post("/identify", mainController.identity);
 
 module.exports = router;
